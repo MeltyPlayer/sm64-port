8801b3199d6e23e8fe5248aaf79069c201d1891f
MeltyPlayer <mastergamepro@hotmail.com>
2020-07-20 16:25:29 -0500

    Added empty class for ScriptBuilder, will be used to encapsulate the LevelScript/BehaviorScript logic.

---
+++ src/common/script/script_builder.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+// TODO: First script: Use CALL() to check if need to rebuild, do so if needed
+//   TODO: Rebuild is necessary if the cache is missing, or if child has 
+//         missing cache/is updated
+//   TODO: Set cache to nullptr when a change is made
+//   TODO: Double-buffer cache (protect against garbage collection)
+// TODO: Second script: Jump to up-to-date script
+class ScriptBuilder {
+};
