7e4cfc3c5270df7a8bfc4ea6b896ca3dbd5ba274
MeltyPlayer <mastergamepro@hotmail.com>
2020-07-23 18:12:36 -0500

    Included some new changes to the validation logic that were accidentally left out of the previous commit.

--- src/common/script/validation_node.cpp
+++ src/common/script/validation_node.cpp
@@ -5,23 +5,32 @@ ValidationNode::~ValidationNode() { clear(); }
 
 bool ValidationNode::is_valid() const { return is_valid_; }
 
+void ValidationNode::validate() { is_valid_ = true; }
+
 void ValidationNode::invalidate() {
   if (!is_valid_) { return; }
 
+  is_valid_ = false;
   for (auto& parent : parents_) {
-    parent->invalidate();
+    if (!parent.expired()) { parent.lock()->invalidate(); }
   }
 }
 
 
 void ValidationNode::clear() {
+  for (auto& child : children_) {
+    if (!child.expired()) {
+      child.lock()->parents_.erase(weak_from_this());
+    }
+  }
   children_.clear();
+
   invalidate();
 }
 
-void ValidationNode::add_dependent(ValidationNode& other) {
-  children_.push_back(&other);
-  other.parents_.push_back(this);
+void ValidationNode::depend_on(std::weak_ptr<ValidationNode> child) {
+  children_.insert(child);
+  child.lock()->parents_.insert(weak_from_this());
 
   invalidate();
 }
--- src/common/script/validation_node.hpp
+++ src/common/script/validation_node.hpp
@@ -1,22 +1,27 @@
 #pragma once
 #include <memory>
-#include <vector>
+#include <set>
 
-class ValidationNode {
+class ValidationNode;
+
+typedef std::owner_less<std::weak_ptr<ValidationNode>> ValidationNodeComparer;
+
+class ValidationNode : public std::enable_shared_from_this<ValidationNode> {
 public:
   ValidationNode() = default;
   ValidationNode(const ValidationNode& other) = delete;
   ~ValidationNode();
 
   bool is_valid() const;
+  void validate();
   void invalidate();
 
   void clear();
-  void add_dependent(ValidationNode& other);
+  void depend_on(std::weak_ptr<ValidationNode> child);
 
 private:
-  std::vector<ValidationNode*> parents_;
-  std::vector<ValidationNode*> children_;
+  std::set<std::weak_ptr<ValidationNode>, ValidationNodeComparer> parents_;
+  std::set<std::weak_ptr<ValidationNode>, ValidationNodeComparer> children_;
 
   bool is_valid_ = false;
 };
